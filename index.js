8dd8aa9a830d75832c62b48a9d8727aea48df9dc
Maxime <maxime.leszczynski@laposte.net>
2019-11-27 11:54:28 +0100

    Commit1

--- index.js
+++ index.js
@@ -6,21 +6,32 @@ const io = require('socket.io')(http)
 app.use(express.static('public'))
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
+
+const messages =[]
 
 io.on('connection', function(socket) {
 
-  const pseudo = socket.handshake.query.pseudo
-  const avatar = socket.handshake.query.avatar
+    const pseudo = socket.handshake.query.pseudo
+    const avatar = socket.handshake.query.avatar
 
-  console.log(`${pseudo} s'est connecté !`)
+    console.log(`${pseudo} s'est connecté !`)
+    socket.on("message",function(value){
+        const data= {avatar:avatar,
+            pseudo:pseudo,
+            message:value,
+            date:Date.now()
+        };
+        messages.push(data)
+        io.emit("message",data)
+    })
 
-  socket.on('disconnect', function () {
-    console.log(`${pseudo} s'est déconnecté !`)
-  })
+    socket.on('disconnect', function () {
+        console.log(`${pseudo} s'est déconnecté !`)
+    })
 })
 
 http.listen(3000, function() {
-  console.log('http://localhost:3000');
+    console.log('http://localhost:3000');
 })
--- public/chat.js
+++ public/chat.js
@@ -1,18 +1,18 @@
 function getHtml (data) {
-  if (typeof data === 'string') {
-    const div = document.createElement('div')
-    div.innerHTML = data
-    data = div
-  }
-  return data
+    if (typeof data === 'string') {
+        const div = document.createElement('div')
+        div.innerHTML = data
+        data = div
+    }
+    return data
 }
 
 function addMessage (data) {
-  document.getElementById('messages').append(getHtml(data))
+    document.getElementById('messages').append(getHtml(data))
 }
 
 function addUser (data) {
-  document.getElementById('users').append(getHtml(data))
+    document.getElementById('users').append(getHtml(data))
 }
 
 const urlParams = new URLSearchParams(window.location.search)
@@ -20,21 +20,38 @@ const pseudo = urlParams.get('pseudo')
 const avatar = urlParams.get('avatar')
 
 const socket = io({
-  query: {
-    pseudo: pseudo,
-    avatar: avatar
-  }
+    query: {
+        pseudo: pseudo,
+        avatar: avatar
+    }
 })
+socket.on('message',function(value){
+    addMessage(`
+        <div class="message">
+        <div class="avatar">
+        <img src="${value.avatar}" width="40px">
+        </div>
+        <div class="content">
+        <div class="pseudo">${value.pseudo}</div>
+        ${value.message}
+        </div>
+        </div>`)
 
-document.querySelector('[data-avatar]').setAttribute('src', avatar)
-document.querySelector('[data-pseudo]').textContent = pseudo
 
-document.getElementById('send').addEventListener('submit', function (e) {
-  e.preventDefault()
-  const value = this.querySelector('input').value
-  if (value) {
-    // Send message
-    console.log(value)
-    this.querySelector('input').value = null
-  }
-})
+    })
+
+
+
+    document.querySelector('[data-avatar]').setAttribute('src', avatar)
+    document.querySelector('[data-pseudo]').textContent = pseudo
+
+    document.getElementById('send').addEventListener('submit', function (e) {
+        e.preventDefault()
+        const value = this.querySelector('input').value
+        if (value) {
+            // Send message
+            socket.emit("message",value)
+            console.log(value)
+            this.querySelector('input').value = null
+        }
+    })
